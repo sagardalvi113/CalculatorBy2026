5031c4a60b979b560fde3c09735f262874366409
sagardalvi113 <91660629+sagardalvi113@users.noreply.github.com>
2026-02-05 19:56:14 +0530

    Modify print statements in Main.java
    Removed duplicate 'Hello, World!' print statement and added 'Hello, World!1' and 'Multiplication3' messages.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!1");
         System.out.println("Hello, World!");
         System.out.println("Subtraction");
         System.out.println("Multiplication3");
