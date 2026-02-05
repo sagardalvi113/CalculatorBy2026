b368de4d59c005c5d62512827795c4fa16dcedfa
sagardalvi113 <91660629+sagardalvi113@users.noreply.github.com>
2026-02-05 19:51:15 +0530

    Change multiplication output message

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
         System.out.println("Subtraction");
-        System.out.println("Multiplication2");
+        System.out.println("Multiplication3");
     }
 }
